913dd531d32a5da681f3011e3ccd55e7d21a9b63
koronisk <koronisk.github@gmail.com>
2026-09-03 14:22:31 +0300

    Serializable PriceList

--- src/main/kotlin/ru/let/csgomarket/pricelist/FloatingPrice.kt
+++ src/main/kotlin/ru/let/csgomarket/pricelist/FloatingPrice.kt
@@ -1,5 +1,8 @@
 package ru.let.csgomarket.pricelist
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class FloatingPrice(
     val lowestPrice: Double,
     val averagePrice: Double,
--- src/main/kotlin/ru/let/csgomarket/pricelist/PriceList.kt
+++ src/main/kotlin/ru/let/csgomarket/pricelist/PriceList.kt
@@ -1,5 +1,8 @@
 package ru.let.csgomarket.pricelist
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 class PriceList<T>(private val prices: Map<String, T>) {
     fun getPrice(itemId: String): T? = prices[itemId]
     fun count(): Int = prices.count()
